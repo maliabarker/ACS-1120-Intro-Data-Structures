95276e38865f4c5b83ca632a732dd99fc5ce3025
maliabarker <89502882+maliabarker@users.noreply.github.com>
2022-02-07 17:17:03 -0800

    implementing dictogram module into flask app

--- Code/app.py
+++ Code/app.py
@@ -1,27 +1,24 @@
 """Main script, uses other modules to generate sentences."""
 from flask import Flask
-from histogram import create_histogram_dict
-from sample import weighted_word
-
+from dictogram import Dictogram
+from file_to_list import read_file
 
 app = Flask(__name__)
-
 
 @app.before_first_request
 def before_first_request():
     """Runs only once at Flask startup"""
     # TODO: Initialize your histogram, hash table, or markov chain here.
-    return create_histogram_dict('example_txt/example1.txt')
-
+    word_list = read_file('example_txt/example1.txt')
+    histogram = Dictogram(word_list)
+    return histogram
 
 @app.route("/")
 def home():
-    histogram = before_first_request()
-    print(histogram)
-    word = weighted_word(histogram)
     """Route that returns a web page containing the generated text."""
+    histogram = before_first_request()
+    word = histogram.sample()
     return word
-
 
 if __name__ == "__main__":
     """To run the Flask server, execute `python app.py` in your terminal.
---
+++ Code/file_to_list.py
@@ -0,0 +1,13 @@
+import random, sys
+from string import punctuation
+
+def read_file(file):
+    with open(file) as f:
+        text = f.read()
+        punct = punctuation + '”“‘’–…'
+        mod_str = ' '.join(filter(None, (word.strip(punct) for word in text.split())))
+        str_list = mod_str.lower().split()
+    return str_list
+
+if __name__ == '__main__':
+    print(read_file('example_txt/example.txt'))
