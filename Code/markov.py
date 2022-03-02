5720a5673c253725212c9a7ac7f68e33256fb0a2
maliabarker <89502882+maliabarker@users.noreply.github.com>
2022-03-01 22:55:14 -0800

    fixing markov chain bugs

--- Code/markov.py
+++ Code/markov.py
@@ -1,49 +1,70 @@
 from cleanup import read_file
 from dictogram import Dictogram
+import random
 
 class MarkovChain(object):
-    """  """
-    def __init__(self, word_list):
+    def __init__(self, word_list, order):
         super(MarkovChain, self).__init__()
         self.corpus = word_list
-        self.markov_dict = {}
+        self.dict = {}
+        self.markov_dict = self.markov_chain(order)
+        
 
-    
     def markov_chain(self, order):
-        for index in range(0, len(self.corpus) - order + 1):
-            # assign variables and check if next_word equals none
-            word = self.corpus[index]
+        word_tuples = []
+        for index in range(0, len(self.corpus) - 1):
+            ### CREATE KEYS ###
+            # initialize empty list to store tuple in
             words = []
+
+            # iterate through numbers by the order given (example, iterate twice for a 2nd order chain)
             for i in range(0, order):
+                # append a word for every n in order to words list
                 if index+i in range(0, len(self.corpus)):
+                    # print(self.corpus[index+i])
                     words.append(self.corpus[index+i])
                 else:
-                    i = 0
-                    words.append(self.corpus[index+i])
-            word_tuple = tuple(words)
-            
-            if index + 1 in range(0, len(self.corpus) - order):
-                next_word = self.corpus[index + order]
-            else:
-                next_word = self.corpus[0]
+                    # index = 0
+                    words.append(self.corpus[index+i - len(self.corpus)])
+                    # print(index+i)
+                    # print(self.corpus[index+i])
+                    
 
-            # check if word is already in markov dictionary
-            if word_tuple in self.markov_dict.keys():
-                # if it is, access dictogram and add next word (will add new key-value pair or increment count)
-                dictogram = self.markov_dict[word_tuple]
+            # create tuple from words list
+            word_tuple = tuple(words)
+            word_tuples.append(word_tuple)
+
+
+            ### CREATE VALUES ###
+            # check if there is a following word 
+            if index+order in range(0, len(self.corpus)):
+                next_word = self.corpus[index+order]
+                # print(f'following word: {self.corpus[index+order]}')
+            # if there is not, loop around to the beginning
+            else:
+                next_word = self.corpus[index+order - len(self.corpus)]
+                # print(f'looping back: {self.corpus[0]}')
+            
+            ### CREATE DICT###
+            # check if the word tuple is already in the dictionary
+            if word_tuple in self.dict.keys():
+                # if it is, access dictogram and add next word
+                dictogram = self.dict[word_tuple]
                 # check if the next word already in dictogram
                 if next_word:
                     if next_word in dictogram.keys():
+                        # if it is, increment count
                         # print(f'{next_word} already there')
                         dictogram.add_count(next_word)
                     else:
                         # print(f'{next_word} not there')
+                        # if it is not, add to value and increment count
                         dictogram.add_count(next_word)
             else:
-                # else create new key value pair with new initalized dictogram
+                # if word tuple not found in dict, create new key value pair with new initalized dictogram
                 if next_word:
-                    self.markov_dict[word_tuple] = Dictogram([next_word])
-        return self.markov_dict
+                    self.dict[word_tuple] = Dictogram([next_word])
+        return self.dict
     
     def walk_chain(self, starting_word, word_count):
         # start with matching key in markov chain as condition
@@ -51,15 +72,25 @@ class MarkovChain(object):
         str = []
         current = starting_word
         while count in range(0, word_count):
-            i = [item for item in self.markov_dict.keys() if current in item]
-            # print(self.markov_dict.keys())
-            # print(i[0])
-            # print(self.markov_dict[i[0]])
-            # if current_word in self.markov_dict.keys():
-                # print('word found')
-            str.append(current)
-            current = self.markov_dict[i[0]].sample()
-            count += 1
+            # i = [item for item in self.markov_dict.keys() if current == item[0]]
+            tuples = []
+            for i in self.markov_dict.keys():
+                # print(f'i is {i}')
+                if current == i[0]:
+                    tuples.append(i)
+            # print(len(tuples))
+            if len(tuples) == 0:
+                return str
+            # print(tuples)
+            random_tuple = random.choice(tuples)
+            # print(f'i used is :{random_tuple}')
+            for n in random_tuple:
+                # print(f'now being appended: {n}')
+                str.append(n)
+                count += 1
+            # print(f'possible options: {self.markov_dict[random_tuple]}')
+            current = self.markov_dict[random_tuple].sample()
+            # print(f'sampled word that is now being used: {current}')
         return str
 
 
@@ -79,11 +110,10 @@ class MarkovChain(object):
 # sample from markov chain's index of current word
 
 
-# remember to use if __name__ == '__main__'
-
-
 if __name__ == '__main__':
-    test = MarkovChain(read_file('example_txt/sherlock-holmes.txt'))
-    test.markov_chain(2)
-    print('___________')
-    print(test.walk_chain('earth', 20))
+    test = MarkovChain(read_file('example_txt/flat_earth.txt'), 5)
+    # print(test.markov_chain(4))
+    # test.markov_chain(3)
+    # print('___________')
+    # print(len(test.markov_chain(3)))
+    print(test.walk_chain('you', 50))
